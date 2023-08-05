47fbac4ba59184dc5fa73ae5666c637e958ae973
r1cardohj <2413302357@qq.com>
2023-08-05 19:26:55 +0800

    v0.1.0

---
+++ README.md
@@ -0,0 +1,11 @@
+# flask-beginner
+
+a quick project directory maker for flask.
+
+**Quick Start**
+
+``` bash
+python -m venv env
+(venv):flask-beginner run -n  
+```
+
---
+++ flask_beginner/__init__.py
@@ -0,0 +1,7 @@
+"""
+    Flask-Beginner
+    -----------------
+    a quick project directory maker for flask.
+    :copyright: (c) 2023 by r1cardohj.
+    :license: MIT, see LICENSE for more details.
+"""
